ae6dd5088eafc0b55d9dc83cd4a9e17c67225fa3
Zeeshan Hooda <zeeshanhooda@icloud.com>
2020-06-16 21:07:12 -0600

    Prettified hex output

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,26 @@
 use std::fs::File;
 use std::io::Read;
 
+fn pretty_hex(value: &u8) -> String {
+    let mut x = format!("{:x}", value);
+    if x.len() < 4 {
+        for _ in 0..4-x.len() {
+            x = format!("0{}", x);
+        }
+    }
+    x
+}
+
+fn dump(data: &Vec<u8>) {
+    for i in 0..data.len() {
+        println!("0x{} : 0x{}", pretty_hex(&(i as u8)), pretty_hex(&data[i]));
+    }
+}
 
 fn main() {
     let mut file = File::open("roms/INVADERS").unwrap();
     let mut data = Vec::<u8>::new();
     file.read_to_end(&mut data);
 
-
+    dump(&data);
 }
